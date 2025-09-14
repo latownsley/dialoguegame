1f646c5f5c4f0da3a8c6b328b9775d6a0b77d487
latownsley <53794433+latownsley@users.noreply.github.com>
2025-09-14 10:08:15 -0700

    Initial Commit

---
+++ player/player.gd
@@ -0,0 +1,40 @@
+extends CharacterBody3D
+
+@onready var animation_player: AnimationPlayer = $Visuals/player/AnimationPlayer
+
+
+const SPEED: float = 5.0
+const JUMP_VELOCITY: float = 4.5
+
+var walking: bool = false
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("left", "right", "forward", "backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+		
+		if !walking:
+			walking = true
+			animation_player.play("walk")
+			
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+		
+		if walking:
+			walking = false
+			animation_player.play("idle")
+
+	move_and_slide()
